7f6d21801c4daa84d7bada1971a6d9d1360b837d
Priscilla Paxton <ppaxton22@turing.edu>
2023-07-08 16:21:48 -0600

    file setup

--- never-ending/src/App.js
+++ never-ending/src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect } from 'react';
 import { getAllWorldsData } from './apiCalls'; 
+import { useDispatch } from 'react-redux';
+import { getDiscoveredWorlds } from './app/rootSlice';
 
 function App() {
+
+const dispatch = useDispatch()
 
   useEffect( () => {
     getAllWorldsData()
     .then(data => {
-      console.log(data)
+      dispatch(getDiscoveredWorlds(data.worlds))
     })
   }, [] )
 
