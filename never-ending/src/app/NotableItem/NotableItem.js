d83ab97c583c5d096fdcd49eeb793b271dc79292
Sharie Trachsel <strachsel22@turing.edu>
2023-07-09 14:33:56 -0500

    Correct errors and added in loading and error handling basics

--- never-ending/src/app/NotableItem/NotableItem.js
+++ never-ending/src/app/NotableItem/NotableItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-export const NotableItem = ({name, type, lore, img, date}) => {
-/// layout is also super janky since i wasn't able to run the App in browser. 
+export const NotableItem = ({name, type, lore, img, date, outcome}) => {
+
   return (
     <div className="notable-card">
       <h3 className="notable-name">{name}</h3>
--- never-ending/src/app/SingleWorld/SingleWorld.js
+++ never-ending/src/app/SingleWorld/SingleWorld.js
@@ -1,53 +1,66 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { getSingleWorldData } from "../../apiCalls";
 import { NotableItem } from "../NotableItem/NotableItem";
 
 export const SingleWorld = () => {
-  const {id} = useParams()
-  const [world, setWorld] = useState({})
-  const notables = world.notableItems.map(item => {
-    const {name, lore, type, img, outcome, population} = item
-    return ( <NotableItem 
-              name = {name}
-              lore = {lore}
-              type = {type}
-              img = {img}
-              outcome = {outcome}
-              population = {population} />
-      )
-    }
-  )
+  const { id } = useParams();
+  const [world, setWorld] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     getSingleWorldData(id)
-    .then(json => {
-      setWorld(json)
-    })
-  }, [])
-  
+      .then((data) => {
+        console.log(data.world)
+        setWorld(data.world);
+        setIsLoading(false);
+      })
+      .catch( err => {
+        setError(true)
+        setIsLoading(false);
+      })
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else if(error){
+    return <div>Sorry the world could not be found</div>
+  }
+
+  const notables = world.notableItems
+    ? world.notableItems.map((item, index) => (
+      <NotableItem
+        key={index}
+        name={item.name}
+        lore={item.lore}
+        type={item.type}
+        img={item.img}
+        outcome={item.outcome}
+        population={item.population}
+      />
+    ))
+    : null;
+
   return (
-    // this lay out is janky as hell and theres no css but it should display
+
     <section className="single-world-view">
       <h2>{world.name}</h2>
-      <img src={world.img}></img>
+      <img src={world.img} alt={world.name} />
       <div className="world-details-box">
-        <div className="longer-lore">{world.longerLore}</div>
+        <div className="longer-lore">LONG LORE: {world.longerLore}</div>
         <div className="bullet-points">
-          <p>{world.magicTechnology.magic}</p>
-          <p>{world.magicTechnology.technologyLevel}</p>
-          <p>{world.geoDynmaics.climate}</p>
-          <p>{world.geoDynmaics.size}</p>
-          <p>{world.dominantRace.name}</p>
-          <p>{world.dominantRace.poltics}</p>
-          <p>{world.geoDynmaics.alignment}</p>
+          <p>Magic: {world.magicTechnology.magic? "Yes": "No"}</p>
+          <p>Genre: {world.magicTechnology.genre}</p>
+          <p>Climate: {world.geoDynamics.climate}</p>
+          <p>Size: {world.geoDynamics.size}</p>
+          <p>Dominant Race: {world.dominantRace.name}</p>
+          <p>Poltics: {world.dominantRace.poltics}</p>
+          <p>Alignment: {world.geoDynamics.alignment}</p>
         </div>
       </div>
-      <section className="notables-box">
-        {notables}
-      </section>
-      <section className="history-box">
-      </section>
+      <section className="notables-box">{notables}</section>
+      <section className="history-box">History</section>
     </section>
-  )
-}
+  );
+};
