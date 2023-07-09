b655a263946ce5cf5360d6742bbcf2b036564b69
Adam Meza <Adammeza95@gmail.com>
2023-07-09 08:27:32 -0400

    add more details to single world view

--- never-ending/src/app/NotableItem/NotableItem.js
+++ never-ending/src/app/NotableItem/NotableItem.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-export const NotableItem = () => {
+export const NotableItem = ({name, type, lore, img, date}) => {
+
   return (
-    <div>
-      
+    <div className="notable-card">
+      <h3 className="notable-name">{name}</h3>
+      <img src={img}></img>
+      <div>
+        <p>type: {type}</p>
+        <p>outcome: {outcome}</p>
+        date
+      </div>
+      <p>lore:{lore}</p>
     </div>
   )
 }
--- never-ending/src/app/SingleWorld/SingleWorld.js
+++ never-ending/src/app/SingleWorld/SingleWorld.js
@@ -1,10 +1,23 @@
 import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { getSingleWorldData } from "../../apiCalls";
+import { NotableItem } from "../NotableItem/NotableItem";
 
 export const SingleWorld = () => {
   const {id} = useParams()
   const [world, setWorld] = useState({})
+  const notables = world.notableItems.map(item => {
+    const {name, lore, type, img, outcome, population} = item
+    return ( <NotableItem 
+              name = {name}
+              lore = {lore}
+              type = {type}
+              img = {img}
+              outcome = {outcome}
+              population = {population} />
+      )
+    }
+  )
 
   useEffect(() => {
     getSingleWorldData(id)
@@ -14,14 +27,20 @@ export const SingleWorld = () => {
   }, [])
   
   return (
+    // this lay out is janky as hell and theres no css but it should display
     <section className="single-world-view">
       <h2>{world.name}</h2>
       <img src={world.img}></img>
-      <div className="world-details">
+      <div className="world-details-box">
         <div className="longer-lore">{world.longerLore}</div>
         <div className="bullet-points"></div>
 
       </div>
+      <section className="notables-box">
+        {notables}
+      </section>
+      <section className="history-box">
+      </section>
     </section>
   )
 }
