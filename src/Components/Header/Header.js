70e9279c6f8be4f6af560d366f4c73e9b53ce75c
Shane Misra <shane.misra@gmail.com>
2023-07-13 15:44:27 -0600

    restore welcomepage before handoff

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,28 @@
 import React from "react";
-import { useHistory } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
+import logo from '../../assets/hl_logo.png'
+import single from '../../assets/single.png'
+import saved from '../../assets/saved.png'
 import './Header.css'
 
 export const Header = () => {
   const history = useHistory();
+  const { id } = useParams();
+  console.log(id)
   const home = () => {
     history.push('/')
   }
+  const explore = () => {
+    history.push('/worlds')
+  }
+  const discover = (id) => {
+    history.push(`/world/${id}`)
+  }
   return (
-    <header className='header-container' onClick={()=>home()}>
-      <h2 className='header-text'>The HyperLoom</h2>
-    </header>
+    <nav className='header-container'>
+      <img className ='header-button explore'src={saved} onClick={()=> explore()} alt='discover button'/>
+      <img className ='header-logo'src={logo} onClick={()=> home()} alt='hyper loom'/>
+      <img className ='header-button discover'src={single} onClick={()=> discover(1)} alt='explore button'/>
+    </nav>
   )
 }
