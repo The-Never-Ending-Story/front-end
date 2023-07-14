429d9cc1fe0566c03437bce1593fdf5c564ebfb5
Sharie Trachsel <122052199+sdtrachsel@users.noreply.github.com>
2023-07-14 09:17:50 -0500

    Add basic styling to Page Not Found

--- src/Components/PageNotFound/PageNotFound.js
+++ src/Components/PageNotFound/PageNotFound.js
@@ -15,10 +15,12 @@ export const PageNotFound = () => {
   }
 
   return (
-    <div className="not-found">
-      <h2> Lost your way?</h2>
-      <p>Sorry, we can't find this page. You'll find worlds to discover and explore back home. </p>
-      <button className="not-found-home" onClick={() => handleClick()}>Home</button>
+    <div className="not-found-wrapper">
+      <div className="not-found">
+        <h2> Lost your way?</h2>
+        <p>Sorry, we can't find this page. You'll find worlds to discover and explore back home. </p>
+        <button className="not-found-home" onClick={() => handleClick()}>Home</button>
+      </div>
     </div>
   )
 }
