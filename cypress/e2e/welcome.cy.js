f6a03bfe3da44c179b03fd7cd74efead6990a4da
Adam Meza <Adammeza95@gmail.com>
2023-07-15 15:02:03 -0400

    welcome page cypress

--- cypress/e2e/welcome.cy.js
+++ cypress/e2e/welcome.cy.js
@@ -5,7 +5,29 @@ describe('Welcome Page', () => {
       fixture: data
     })
   }).visit('http://localhost:3000')
-})
-  it('', () => {
-    cy
-})
+  
+  it('should display correct initial text', () => {
+    cy.get('.initial-text').should('have.text', 'Introducing HyperLoom');
+  });
+
+  it('should display correct intro text', () => {
+    cy.get('.intro-text').should(
+      'have.text',
+      'HyperLoom uses the power of MidJourney and ChatGPT to create new and exciting worlds, complete with rich lore. Explore barely known worlds, or discover new domains:'
+    );
+  });
+
+  it('should navigate to correct URLs when links are clicked', () => {
+    cy.contains('Explore').click();
+    cy.url().should('include', '/worlds');
+
+    cy.go('back');
+
+    cy.contains('Create').click();
+    cy.url().should('include', '/world/2');
+  });
+
+  it('should contain the button container', () => {
+    cy.get('.button-container').should('exist');
+  });
+});
