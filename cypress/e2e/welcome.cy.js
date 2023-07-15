9706b32f7575ca2267f587d0e2a40be6efbd1667
Adam Meza <Adammeza95@gmail.com>
2023-07-15 16:14:33 -0400

    finish cypress test

--- cypress/e2e/welcome.cy.js
+++ cypress/e2e/welcome.cy.js
@@ -2,9 +2,9 @@ describe('Welcome Page', () => {
   beforeEach(()=> {
     cy.intercept('https://hyperloom-d209dae18b26.herokuapp.com/worlds?format=json', {
       statusCode: 200,
-      fixture: data
-    })
-  }).visit('http://localhost:3000')
+      fixture: 'data'
+    }).visit('http://localhost:3000')
+  })
   
   it('should display correct initial text', () => {
     cy.get('.initial-text').should('have.text', 'Introducing HyperLoom');
