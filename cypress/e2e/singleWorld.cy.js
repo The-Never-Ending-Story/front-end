448dcec5a35c6fae22ae8e95eeb75092db31c6f1
Sharie Trachsel <122052199+sdtrachsel@users.noreply.github.com>
2023-07-16 17:37:27 -0500

    Fix cypress test for SingleWorld styling changes

--- cypress/e2e/singleWorld.cy.js
+++ cypress/e2e/singleWorld.cy.js
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('Single World View', () => {
   beforeEach(()=> {
     cy.intercept('https://hyperloom-d209dae18b26.herokuapp.com/worlds?format=json', {
       statusCode: 200,
@@ -22,21 +22,18 @@ describe('template spec', () => {
       .should('have.attr', 'src')
       .and('include', "https://cdn.discordapp.com/attachments/1128814452012220536/1129842151103148173/hyperloom_DigiVale_is_a_vivid_blend_of_natural_beauty_magic-inf_44ee86aa-76d6-416b-9526-75abd158ae6b.png");
 
-    cy.get('h1').should('have.text', "DigiVale"); // Fill in the expected text for the world name
-
+    cy.get('h1').should('have.text', "DigiVale"); 
     cy.get('.single-geo').should('exist');
     cy.get('.attr-name').eq(0).should('have.text', 'Shape ')
-    cy.get('.attr-name').eq(1).should('have.text', 'Size: ')
-    cy.get('.attr-name').eq(2).should('have.text', 'Climate: ')
-    cy.get('.attr-name').eq(3).should('have.text', 'Magic: ')
-    cy.get('.attr-name').eq(4).should('have.text', 'Level: ')
-    cy.get('.attr-name').eq(5).should('have.text', 'Techonology: ')
-    cy.get('.attr-name').eq(6).should('have.text', 'Level: ')
-
+    cy.get('.attr-name').eq(1).should('have.text', 'Size ')
+    cy.get('.attr-name').eq(2).should('have.text', 'Climate ')
+    cy.get('.lvl-name').eq(0).should('have.text', 'MagicLevel 10')
+    cy.get('.lvl-detail').eq(0).should('have.text', 'Virtually limitless, advanced enchantments and cosmic wizardry')
+    cy.get('.lvl-name').eq(1).should('have.text', 'TechonologyLevel 4')
+    cy.get('.lvl-detail').eq(1).should('have.text', 'Basic ai, primitive cybernetics and rudimentary nanotech')
     cy.contains("Planet")
     cy.contains("DigiVale is a vivid blend of natural beauty, magic-infused landscapes, and cyberpunk aesthetics. Shimmering salt flats stretch for miles, dotted with conductive crystals that spin in harmony, enhancing magic for miles. The savanna carries vibrant flora and fauna, pulsating with otherworldly life, while its vast, cavernous caves, crisscrossing underneath, are both mystic and perilous.")
-    cy.contains("virtually limitless, advanced enchantments, and cosmic wizardry")
-    cy.contains("basic AI, primitive cybernetics, and rudimentary nanotech")
+
   });
 
   it('should display the inhabitants tab and check its elements', () => {
