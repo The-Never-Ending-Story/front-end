6cead749aa3c75301d3a701bc8687769b86e41d9
Sharie Trachsel <122052199+sdtrachsel@users.noreply.github.com>
2023-07-17 08:56:16 -0500

    Fix functionality and update styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,29 +1,49 @@
 import React from "react";
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useHistory } from 'react-router-dom'
+import { getRandomWorldData } from "../../apiCalls";
+import { useSelector, useDispatch } from 'react-redux';
+import { getDiscoveredWorlds, changeError } from '../rootSlice';
 import logo from '../../assets/hl_logo.png'
 import './Header.css'
 
 export const Header = () => {
-const {pathname} = useLocation();
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const displayedWorlds = useSelector((state) => state.root.discoveredWorlds)
 
-if (pathname !== '/' && pathname !== '/worlds') {  
+  const discoverNewWorld = ()=> {
+    getRandomWorldData()
+    .then(data=> {
+      const addOne = [...displayedWorlds, data]
+      dispatch(getDiscoveredWorlds(addOne))
+      history.push(`/world/${data.id}`)
+    }).catch((error)=>{
+      dispatch(changeError(error))
+    })
+  }
+
+  const {pathname} = useLocation();
+
   return (
     <nav className='header-container'>
-      <Link to='/'>
-        <img className ='header-logo'src={logo} alt='hyper loom'/>
-      </Link>
-      <Link to='/worlds'>
-        <button className="header-button">Explore</button>
-      </Link>
+      <div className='logo-container'>
+        <Link to='/'>
+          <img className ='header-logo'src={logo} alt='hyper loom'/>
+        </Link>
+      </div>
+      <div className='buttons-container'>
+        {pathname === '/worlds' && (
+          <button className="header-button" onClick={discoverNewWorld}>Create</button>
+        )}
+        {pathname.includes('/world/') && (
+          <>
+            <Link to='/worlds'>
+              <button className="header-button">Explore</button>
+            </Link>
+            <button className="header-button" onClick={discoverNewWorld}>Create</button>
+          </>
+        )}
+      </div>
     </nav>
   )
-} else {
-  return (
-  <nav className='header-container'>
-    <Link to='/'>
-    <img className ='header-logo'src={logo} alt='hyper loom'/>
-    </Link>
-  </nav>
-  )
-}
 }
