09d292a08fb399a9f29910b1aae326ce5ff0666d
Shane Misra <shane.misra@gmail.com>
2023-07-16 20:30:19 -0600

    hidden discover button until rework on that fetch, moved explore to middle under home

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,31 @@
 import React from "react";
-import { Link, useParams } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import logo from '../../assets/hl_logo.png'
 import './Header.css'
 
 export const Header = () => {
-const {id} = useParams()
-console.log(id)
+const {pathname} = useLocation();
+console.log(pathname)
+
+
+if (pathname !== '/') {  
   return (
     <nav className='header-container'>
-      <Link to='/worlds'>
-        <button className="header-button">Explore</button>
-      </Link>
       <Link to='/'>
         <img className ='header-logo'src={logo} alt='hyper loom'/>
       </Link>
-      <Link to={`/world/`}>
-          <button className="header-button"></button>
+      <Link to='/worlds'>
+        <button className="header-button">Explore</button>
       </Link>
     </nav>
   )
+} else {
+  return (
+  <nav className='header-container'>
+    <Link to='/'>
+    <img className ='header-logo'src={logo} alt='hyper loom'/>
+    </Link>
+  </nav>
+  )
+}
 }
