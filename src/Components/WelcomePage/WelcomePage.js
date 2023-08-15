b2a69dd512bf2cbf1f2c953962d881d54a743aad
Shane Misra <shane.misra@gmail.com>
2023-08-15 10:56:37 -0600

    begin animations for text boxes, no other changes

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from 'framer-motion'; 
 import { Link, useHistory } from 'react-router-dom'
 import { getRandomWorldData } from "../../apiCalls";
 import { useSelector, useDispatch } from 'react-redux';
@@ -22,29 +23,57 @@ export const WelcomePage = () => {
       dispatch(changeError(error));
     })
   };
+  const sentence = {
+    hidden: {opacity:0},
+    visible: {
+      opacity: 1,
+      transition: {
+        delay:.25,
+        duration:3.75,
+        staggerChildren: 0.25,
+      }
+    }
+  }
+
+  const introText ='HyperLoom uses the power of MidJourney and ChatGPT to create new and exciting worlds, complete with rich lore. Explore barely known worlds, or discover new domains:'
+  const letter = {
+    hidden: {opacity: 0},
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 3.5,
+        duration: 3,
+      }
+    }
+  }
 
   if (error) {
     return <Error />;
   };
-
+// animate intro text, welcome menu itself upon arrival, and iterate through a selection of the backgrounds once the api call is made
   return (
-    <main className="welcome-page">
-      <section className="welcome-menu">
-        <div className="initial-text">
+    <main className="welcome-page"
+    >
+      <motion.section className="welcome-menu" variants={sentence} initial='hidden' animate='visible'>
+        <motion.div className="initial-text" variants={sentence} initial='hidden' animate='visible'>
           Introducing HyperLoom
-        </div>
-        <p className="intro-text">
-          HyperLoom uses the power of MidJourney and ChatGPT to create new and exciting worlds, complete with rich lore. 
-
-          Explore barely known worlds, or discover new domains:
-        </p>
+        </motion.div>
+        <motion.p className="intro-text" variants={letter} initial='hidden' animate='visible'>
+          {introText.split('').map((char, index)=> {
+            return (
+            <motion.span key={char+'-'+index}>
+              {char}
+            </motion.span>
+            )
+          })}
+        </motion.p>
         <div className="button-container">
           <Link to='/worlds'>
             <button className="menu-button">Explore</button>
           </Link>
           <button className="menu-button" onClick={()=> {discoverNewWorld()}}>Discover</button>
         </div>
-      </section>
+      </motion.section>
     </main>
   )
 };
