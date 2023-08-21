06d453d3a3657ea18f6f0b2465fca0b1e557d35d
Shane Misra <shane.misra@gmail.com>
2023-08-21 13:08:45 -0600

    no warning commit, decent structure but no styling

--- src/Components/HeroCarousel/HeroCarousel.js
+++ src/Components/HeroCarousel/HeroCarousel.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './HeroCarousel.css'
 
-const HeroCarousel = () => {
+const HeroCarousel = ({worldPreviews}) => {
+  useEffect(()=> {
+    if (!worldPreviews.length > 0) {
+      console.log('worldPreviews empty')
+    }
+  }, [worldPreviews])
+
   return (
     <section className='my-hero'>
       <div className='test-words'>
         Aetheria
+      <div className="keyframe-arrow">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
       </div>
     </section>
   )
--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -3,7 +3,6 @@ import { Link, useHistory } from 'react-router-dom'
 import { getRandomWorldData } from "../../apiCalls";
 import { useSelector, useDispatch } from 'react-redux';
 import { getDiscoveredWorlds, changeError } from '../rootSlice';
-import WelcomeModal from './WelcomeModal'
 import { Error } from '../Error/Error'
 import { motion } from 'framer-motion'; 
 import './WelcomePage.css'
@@ -46,35 +45,46 @@ export const WelcomePage = () => {
       }
     }
   }
-  const slowFadeIn = {
-    hidden: { opacity: 0 },
+
+  const revealFromLeft = {
+    hidden: {opacity: 0, marginRight:'10rem'},
     visible: {
       opacity: 1,
+      marginRight:'3rem',
       transition: {
-        delay: 3,
-        duration: 3,
-        staggerChildren: 1.75,
+        delay: .15,
+        duration: 3
       }
     }
-  };
-
+  }
   if (error) {
     return <Error />;
   };
 
   return (
     <main className="welcome-page" >
-      <HeroCarousel/>
+      <HeroCarousel worldPreviews={worldPreviews}/>
       <motion.div className='first-box' variants={fadeInRise} initial='hidden' animate='visible'>
-        <motion.span className='first-intro-text'>
+        <motion.span className='first-intro-text' >
           Introducing Hyperloom
         </motion.span>
+        <div className="button-container">
+          <Link to='/worlds'>
+            <button className="menu-button">Explore</button>
+          </Link>
+          <button className = 'menu-button'onClick={()=>setAboutModal(!aboutModalVisible)}>?</button>
+          <button className="menu-button" onClick={()=> {discoverNewWorld()}}>Discover</button>
+        </div>
       </motion.div>
-      <motion.div className='second-box' variants={slowFadeIn} initial='hidden' animate='visible'>
-        <motion.span className='second-intro-text'>
+        {aboutModalVisible && 
+        <motion.span 
+        className='second-intro-text' 
+        variants={revealFromLeft}
+        initiate='hidden' 
+        animate='visible'>
           Explore new worlds built with ChatGPT and MidJourney, rich with inhabitants, histories, and futures yet unknown...
         </motion.span>
-      </motion.div>
+        }
     </main>
   )
 };
