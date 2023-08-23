f9c91fae570088fcad41aca8b76cbecbd58d1a8b
Shane Misra <shane.misra@gmail.com>
2023-08-23 08:35:02 -0600

    create general structure and match spec, unfinished animations

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { Link, useHistory } from 'react-router-dom'
+import React from "react";
+import { useHistory } from 'react-router-dom'
 import { getRandomWorldData } from "../../apiCalls";
 import { useSelector, useDispatch } from 'react-redux';
 import { getDiscoveredWorlds, changeError } from '../rootSlice';
@@ -14,7 +14,6 @@ export const WelcomePage = () => {
   const history = useHistory();
   const displayedWorlds = useSelector((state) => state.root.discoveredWorlds);
   const error = useSelector((state) => state.root.error);
-  const [aboutModalVisible, setAboutModal] = useState(false)
 
   const discoverNewWorld = ()=> {
     getRandomWorldData()
@@ -27,57 +26,30 @@ export const WelcomePage = () => {
     })
   };
 
-  const fadeInRise = {
-    hidden: { opacity: 0},
-    visible: {
-      opacity: 1, 
-      transition: {
-        delay:.25,
-        duration:3,
-        staggerChildren: 0.25,
-      }
-    }
-  }
-
-  const revealFromLeft = {
-    hidden: {opacity: 0, marginRight:'10rem'},
-    visible: {
-      opacity: 1,
-      marginRight:'3rem',
-      transition: {
-        delay: .15,
-        duration: 3
-      }
-    }
-  }
   if (error) {
     return <Error />;
   };
 
+  const fadeInMoveRight = {
+    hidden: { opacity: 0, left: '0rem'},
+    visible: {
+      opacity: 1,
+      left: '5rem', 
+      transition: {
+        delay:.25,
+        duration:5,
+      }
+    }
+  }
+
   return (
     <main className="welcome-page" >
       <HeroImageSlider/>
-      <motion.div className='first-box' variants={fadeInRise} initial='hidden' animate='visible'>
-        <motion.span className='first-intro-text' >
-          Introducing Hyperloom
+      <motion.div className='intro-box' variants={fadeInMoveRight} initial='hidden' animate='visible'>
+        <motion.span className='intro-text'>
+          Welcome to HyperLoom
         </motion.span>
-        <div className="button-container">
-          <Link to='/worlds'>
-            <button className="menu-button">Explore</button>
-          </Link>
-          <button className = 'menu-button'onClick={()=>setAboutModal(!aboutModalVisible)}>?</button>
-          <button className="menu-button" onClick={()=> {discoverNewWorld()}}>Discover</button>
-        </div>
       </motion.div>
-        {aboutModalVisible && 
-        <motion.span 
-        className='second-intro-text' 
-        variants={revealFromLeft}
-        initiate='hidden' 
-        animate='visible'>
-          Explore new worlds built with ChatGPT and MidJourney, rich with inhabitants, histories, and futures yet unknown...
-        </motion.span>
-        }
       <section className='welcome-bottom'>
       <About discoverNewWorld={discoverNewWorld}/>
       </section>
